7f2c9aa4fefad8cc42e253e83c06a245c61c1d0a
Harishpolina <49839580+Harishpolina@users.noreply.github.com>
2019-04-25 11:03:55 +0530

    Add files via upload
    This is the complete progress of preemtive priority scheduling , this code is similar to part4 but the difference is the way of displaying options.

---
+++ fin.c
@@ -0,0 +1,163 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct process
+{
+int i,j,n,count,slice;
+int x[500],y[500],pr[500],tat[500],wat[500],rem_bt[500];
+}obj;
+void getdata()
+{	
+system("clear");
+sleep(1);
+printf("********************************************WELCOME TO PREEMTIVE PRIORITY SCHEDULING ALGORITHM************************************************\n\n");
+printf("Enter the number of processes you want : ");
+scanf("%d",&obj.n);
+if(obj.n<=0)
+{
+printf("Invalid Input try again\n");
+getdata();
+}
+for(obj.i=1;obj.i<=obj.n;obj.i++)
+{
+printf("\n Enter the Details for Process [%d]:",obj.i);
+printf("\nBurst Time : %d : ",obj.i);
+scanf("%d",&obj.y[obj.i]);
+if(obj.y[obj.i]<=0)
+{
+printf("Invalid Input try again\n");
+getdata();		
+}
+printf("\nArrival Time : ");
+scanf("%d",&obj.x[obj.i]);
+if(obj.x[obj.i]<0)
+{
+printf("Invalid Input try again\n");
+getdata();		
+}
+printf("\nPriority : ");
+scanf("%d",&obj.pr[obj.i]);
+if(obj.pr[obj.i]<0)
+{
+printf("Invalid Input try again\n");
+getdata();
+}
+}
+}
+void display()
+{
+printf("Process ID\t\t\tBurst Time\t\t\tArrival Time\t\t\tPriority\n");
+for(obj.i=1;obj.i<=obj.n;obj.i++)
+{
+printf("    %d\t\t\t\t    %d\t\t\t\t     %d\t\t\t\t    %d\n",obj.i,obj.y[obj.i],obj.x[obj.i],obj.pr[obj.i]);
+}
+}
+void priority()
+{
+obj.count=0;
+if(obj.pr[obj.i]==0)
+{
+obj.pr[obj.i]+=1;
+obj.count++;
+for(obj.i=1;obj.i<=obj.n;obj.i++)
+{
+obj.count=1;
+if(obj.pr[obj.i]!=obj.count)
+{
+obj.pr[obj.i]+=2;
+}
+}
+}
+}
+
+
+
+
+//---------------------END for all the inputs given by the user---------------------
+
+
+
+
+void timeslice()
+{
+obj.slice=1;
+int t=0;
+for(obj.i=1;obj.i<=obj.n;obj.i++)
+{
+obj.rem_bt[obj.i]=obj.y[obj.i];
+}
+while(1)
+{
+int flag=1;
+for(obj.i=1;obj.i<=obj.n;obj.i++)
+if(obj.rem_bt[obj.i]>0)
+{
+flag=0;
+if(obj.rem_bt[obj.i]>obj.slice)
+{
+t+=obj.slice;
+obj.rem_bt[obj.i]-=obj.slice;
+}
+else
+{
+t=t+obj.rem_bt[obj.i];
+obj.wat[obj.i]=t-obj.y[obj.i];
+obj.rem_bt[obj.i]=0;
+}
+}
+if(flag==1)
+break;
+}
+}
+void turnaround()
+{
+printf("\n****************************************************TURNAROUND TIME AND WAITING TIME**********************************************************\n");
+printf("\nProcess ID\tBurst Time\tArrival Time\tPriority\tTurnaround Time\t  Waiting Time\n");
+for(obj.i=1;obj.i<=obj.n;obj.i++)
+{
+obj.tat[obj.i]=obj.wat[obj.i]+obj.y[obj.i];
+printf("   %d\t\t   %d\t\t    %d\t\t   %d\t\t   %d\t\t  %d\t\t\n",obj.i,obj.y[obj.i],obj.x[obj.i],obj.pr[obj.i],obj.tat[obj.i],obj.wat[obj.i]);
+}
+}
+void waiting()
+{
+for(obj.i=1;obj.i<=obj.n;obj.i++)  
+{
+return turnaround();
+}
+}
+
+
+
+
+int main()
+{ 	
+ float k=0,m=0;
+ long int q,u;
+ printf("Enter ID:-");v:
+ scanf("%ld",&q);
+ printf("Enter Password:-");
+ scanf("%ld",&u);
+ if(q==11716063 && u==12345)
+ {
+ getdata();
+ display();
+ priority();
+ timeslice();
+ turnaround();
+ }
+ else 	
+ {
+ printf("enter the correct details\n");
+ goto v;
+ }
+ for(obj.i=1;obj.i<=obj.n;obj.i++)
+ {
+ k=k+obj.tat[obj.i];
+ m=m+obj.wat[obj.i];
+ }
+ k=k/obj.n;
+ m=m/obj.n;
+ printf("\nAverage turnaround time : %f\n",k);
+ printf("\nAverage waiting time : %f\n",m);
+ }
+
